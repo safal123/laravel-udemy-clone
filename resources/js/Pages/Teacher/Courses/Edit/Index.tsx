8bff82d87d75c96edf73f9ad8e5d442ee2c82b80
Safal Pokharel <pokharelsafal66@gmail.com>
2024-07-31 18:42:30 +1000

    updated course form

--- resources/js/Pages/Teacher/Courses/Edit/Index.tsx
+++ resources/js/Pages/Teacher/Courses/Edit/Index.tsx
@@ -1,51 +1,21 @@
 import TeacherDashboardLayout from "@/Layouts/TeacherDashboardLayout";
-import React, {useEffect, useState} from "react";
-import {Head, Link, router, useForm, usePage} from "@inertiajs/react";
+import React, {useState} from "react";
+import {Head, Link, router, usePage} from "@inertiajs/react";
 import {Course} from "@/types";
-import FieldGroup from "@/Components/shared/form/FieldGroup";
-import LoadingButton from "@/Components/shared/button/LoadingButton";
-import TextInput from "@/Components/shared/form/TextInput";
 import {Button} from "@/Components/ui/button";
-import TextareaInput from "@/Components/shared/form/TextareaInput";
 import axios from "axios";
-import SelectInput from "@/Components/shared/form/SelectInput";
-import slugify from "slugify";
-// @ts-ignore
-import {truncate} from "lodash";
 import AddChapter from "@/Pages/Teacher/Courses/Edit/Partials/AddChapter";
 import ChaptersTable from "@/Pages/Teacher/Courses/Edit/Partials/ChaptersTable";
 import {Loader} from "lucide-react";
-import UploadCourseImage from "@/Pages/Teacher/Courses/Edit/Partials/UploadCourseImage";
 import {toast} from "sonner";
+import CourseForm from "@/Pages/Teacher/Courses/Edit/Partials/CourseForm";
 
 const Edit = () => {
   const {course} = usePage<{ course: Course }>().props
   const {categories} = usePage<any>().props
-  const {chapters} = usePage<any>().props
-  const [previewImageUrl, setPreviewImageUrl] = useState<string>('')
-  const [objectUrl, setObjectUrl] = useState<string>('')
-  const [uploading, setUploading] = useState<boolean>(false)
-  const [courseFile, setCourseFile] = useState<File | null>(null)
   const [isPublishing, setIsPublishing] = useState<boolean>(false)
 
-  const {data, setData, errors, put, processing} = useForm({
-    'title': course.title || '',
-    'description': course.description || '',
-    'price': course.price,
-    'image_storage_id': course.image_storage_id || '',
-    'category_id': course.category_id || '',
-    'slug': course.slug || '',
-  })
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    put(route('teachers.courses.update', course.id))
-  }
-
-  useEffect(() => {
-    setData('slug', slugify(data.title, {lower: true}))
-  }, [data.title])
-
-  const handleChapterPublish = async () => {
+  const handleCoursePublish = async () => {
     try {
       setIsPublishing(true)
       await axios.put(route('teachers.courses.toggle-publish', course.id))
@@ -71,12 +41,12 @@ const Edit = () => {
           >
             Courses
           </Link>
-          <span className="font-medium text-gray-600"> /</span> {data.title}
+          <span className="font-medium text-gray-600"> /</span> {course.title}
         </h1>
         <div className="ml-auto flex items-center">
           <Button
             variant={course.is_published ? 'default' : 'outline'}
-            onClick={handleChapterPublish}
+            onClick={handleCoursePublish}
           >
             {isPublishing && <Loader className={'animate-spin mr-2'} size={20}/>}
             {course.is_published ? 'Unpublish' : 'Publish'}
@@ -91,77 +61,7 @@ const Edit = () => {
         </div>
       </div>
       <div className="overflow-hidden bg-white rounded shadow">
-        <form onSubmit={handleSubmit} className={'flex flex-col lg:flex-row h-full items-stretch'}>
-          <div className="w-full lg:w-1/2 flex-1 min-h-full h-full">
-            <div className="grid gap-8 p-8 lg:grid-cols-2">
-              <FieldGroup label="Course Title" name="title" error={errors.title}>
-                <TextInput
-                  name="name"
-                  error={errors.title}
-                  value={data.title}
-                  onChange={e => setData('title', e.target.value)}
-                />
-              </FieldGroup>
-              <FieldGroup label="Course Price" name="price" error={errors.price}>
-                <TextInput
-                  name="price"
-                  type={'number'}
-                  error={errors.price}
-                  value={data.price}
-                  onChange={(e: any) => setData('price', parseFloat(e.target.value))}
-                />
-              </FieldGroup>
-              <FieldGroup label="Course Slug" name="price" error={errors.slug}>
-                <TextInput
-                  name="slug"
-                  error={errors.slug}
-                  disabled={true}
-                  value={data.slug}
-                  onChange={(e: any) => setData('slug', e.target.value)}
-                />
-              </FieldGroup>
-              <FieldGroup label="Course Category" name="category_id" error={errors.category_id}>
-                <SelectInput
-                  name="category_id"
-                  error={errors.category_id}
-                  value={data.category_id}
-                  onChange={e => setData('category_id', e.target.value)}
-                  options={categories.map((category: any) => ({
-                    label: category.name,
-                    value: category.id
-                  }))}
-                />
-              </FieldGroup>
-            </div>
-            <div className={'px-6'}>
-              <FieldGroup label="Course Description" name="title" error={errors.description}>
-                <TextareaInput
-                  name="description"
-                  error={errors.description}
-                  value={data.description}
-                  onChange={e => setData('description', e.target.value)}
-                />
-              </FieldGroup>
-            </div>
-            <div className="flex items-center px-6 py-4">
-              <LoadingButton
-                loading={processing}
-                type="submit"
-                className="btn-indigo"
-              >
-                {processing ? 'Updating...' : 'Update Course'}
-              </LoadingButton>
-            </div>
-          </div>
-          <div className="w-full lg:w-1/2 flex-1">
-            <div className={'px-6 py-4 min-h-[400px]'}>
-              <UploadCourseImage
-                errors={errors}
-                course={course}
-              />
-            </div>
-          </div>
-        </form>
+        <CourseForm course={course} categories={categories}/>
       </div>
       <div className={'mt-6'}>
         <div className={'flex items-center justify-between py-4'}>
---
+++ resources/js/Pages/Teacher/Courses/Edit/Partials/CourseForm.tsx
@@ -0,0 +1,108 @@
+import FieldGroup from "@/Components/shared/form/FieldGroup";
+import TextInput from "@/Components/shared/form/TextInput";
+import SelectInput from "@/Components/shared/form/SelectInput";
+import TextareaInput from "@/Components/shared/form/TextareaInput";
+import LoadingButton from "@/Components/shared/button/LoadingButton";
+import UploadCourseImage from "@/Pages/Teacher/Courses/Edit/Partials/UploadCourseImage";
+import React, {useEffect} from "react";
+import {useForm} from "@inertiajs/react";
+import {Course} from "@/types";
+import slugify from "slugify";
+
+type CourseFormProps = {
+  course: Course;
+  categories: any;
+}
+
+const CourseForm = ({ course, categories }: CourseFormProps) => {
+  const {data, setData, errors, put, processing} = useForm({
+    'title': course.title || '',
+    'description': course.description || '',
+    'price': course.price,
+    'image_storage_id': course.image_storage_id || '',
+    'category_id': course.category_id || '',
+    'slug': course.slug || '',
+  })
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    put(route('teachers.courses.update', course.id))
+  }
+  useEffect(() => {
+    setData('slug', slugify(data.title, {lower: true}))
+  }, [data.title])
+  return (
+    <form onSubmit={handleSubmit} className={'flex flex-col lg:flex-row h-full items-stretch'}>
+      <div className="w-full lg:w-1/2 flex-1 min-h-full h-full">
+        <div className="grid gap-8 p-8 lg:grid-cols-2">
+          <FieldGroup label="Course Title" name="title" error={errors.title}>
+            <TextInput
+              name="title"
+              error={errors.title}
+              value={data.title}
+              onChange={e => setData('title', e.target.value)}
+            />
+          </FieldGroup>
+          <FieldGroup label="Course Price" name="price" error={errors.price}>
+            <TextInput
+              name="price"
+              type={'number'}
+              error={errors.price}
+              value={data.price}
+              onChange={(e: any) => setData('price', parseFloat(e.target.value))}
+            />
+          </FieldGroup>
+          <FieldGroup label="Course Slug" name="price" error={errors.slug}>
+            <TextInput
+              name="slug"
+              error={errors.slug}
+              disabled={true}
+              value={data.slug}
+              onChange={(e: any) => setData('slug', e.target.value)}
+            />
+          </FieldGroup>
+          <FieldGroup label="Course Category" name="category_id" error={errors.category_id}>
+            <SelectInput
+              name="category_id"
+              error={errors.category_id}
+              value={data.category_id}
+              onChange={e => setData('category_id', e.target.value)}
+              options={categories.map((category: any) => ({
+                label: category.name,
+                value: category.id
+              }))}
+            />
+          </FieldGroup>
+        </div>
+        <div className={'px-6'}>
+          <FieldGroup label="Course Description" name="description" error={errors.description}>
+            <TextareaInput
+              name="description"
+              error={errors.description}
+              value={data.description}
+              onChange={e => setData('description', e.target.value)}
+            />
+          </FieldGroup>
+        </div>
+        <div className="flex items-center px-6 py-4">
+          <LoadingButton
+            loading={processing}
+            type="submit"
+            className="btn-indigo"
+          >
+            {processing ? 'Updating...' : 'Update Course'}
+          </LoadingButton>
+        </div>
+      </div>
+      <div className="w-full lg:w-1/2 flex-1">
+        <div className={'px-6 py-4 min-h-[400px]'}>
+          <UploadCourseImage
+            errors={errors}
+            course={course}
+          />
+        </div>
+      </div>
+    </form>
+  )
+}
+
+export default CourseForm;
