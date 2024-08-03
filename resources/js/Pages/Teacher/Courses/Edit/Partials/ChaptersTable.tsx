f59bd8150292dc3a156c221b27826335ea7b985a
Safal Pokharel <pokharelsafal66@gmail.com>
2024-08-03 20:05:13 +1000

    check if the chapters table is causing an issue for 502

--- resources/js/Pages/Teacher/Courses/Edit/Partials/ChaptersTable.tsx
+++ resources/js/Pages/Teacher/Courses/Edit/Partials/ChaptersTable.tsx
@@ -1,33 +1,11 @@
 import React from "react";
-import UpdateChapter from "@/Pages/Teacher/Courses/Edit/Partials/UpdateChapter";
-import {Chapter} from "@/types";
-import DeleteChapter from "@/Pages/Teacher/Courses/Edit/Partials/DeleteChapter";
-import ChapterVideo from "@/Pages/Teacher/Courses/Edit/Partials/ChapterVideo";
-import ChapterTogglePublish from "@/Pages/Teacher/Courses/Edit/Partials/ChapterTogglePublish";
-import ToggleChapterFree from "@/Pages/Teacher/Courses/Edit/Partials/ToggleChapterFree";
 
-type ChaptersTableProps = {
-  chapters: Chapter
-}
-
-const ChaptersTable = ({chapters}: ChaptersTableProps) => {
+const ChaptersTable = () => {
   return (
     <div>
       <div className={'ml-auto flex items-center'}>
         I am ChaptersTable
       </div>
-      {/*{chapters.map((chapter) => (*/}
-      {/*  <div key={chapter.id} className={'flex items-center space-x-4 mb-4'}>*/}
-      {/*    {chapter.title}*/}
-      {/*    <div className={'ml-auto flex items-center'}>*/}
-      {/*      <UpdateChapter chapter={chapter}/>*/}
-      {/*      <DeleteChapter chapter={chapter}/>*/}
-      {/*      <ChapterVideo chapter={chapter}/>*/}
-      {/*      <ChapterTogglePublish chapter={chapter}/>*/}
-      {/*      <ToggleChapterFree chapter={chapter}/>*/}
-      {/*    </div>*/}
-      {/*  </div>*/}
-      {/*))}*/}
     </div>
   )
 }
