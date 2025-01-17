02c8a533138a29c0cc4e1af5fc03f1915a25e34b
Safal Pokharel <pokharelsafal66@gmail.com>
2025-01-17 20:17:11 +1100

    fix build

--- public/build/assets/CourseChapters-DN3Kw4wt.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-bkc-gmOT.js";const e=()=>r.jsx("div",{children:"Course Chapters"});export{e as default};
--- public/build/assets/index-JPL_TFbB.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./app-bkc-gmOT.js";function f(u){const e=t.useRef(u);return t.useEffect(()=>{e.current=u}),t.useMemo(()=>(...r)=>{var o;return(o=e.current)==null?void 0:o.call(e,...r)},[])}var a=globalThis!=null&&globalThis.document?t.useLayoutEffect:()=>{};export{a,f as u};
